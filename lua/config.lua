c372ad00f81734ddcd6a3e43b19a2eb388274249
Joseph Agrane <josephagrane@gmail.com>
2021-04-20 10:15:39 +0100

    turned off debug mode in config

--- lua/config.lua
+++ lua/config.lua
@@ -4,11 +4,11 @@ windowHeight = 720
 windowTitle = "AH Project Joseph Agrane"
 gameVersion = "1.0.0"
 
-firstLevel = "lua/scripts/levels/breakout.lua"
+--firstLevel = "lua/scripts/levels/breakout.lua"
 firstLevel = "lua/scripts/levels/welcome.lua"
 
 physicsLayerCount = 5
 
 backgroundColour = Colour.white
 
-debugMode = true
+debugMode = false
